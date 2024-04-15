913b5439fd3266c819517c553b1f9270cbd84bc4
Nikita Prebyshevskiy <legathunter@mail.ru>
2024-04-15 21:00:52 +0300

    переходной

--- Server/index.js
+++ Server/index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const sequelize = require('./db')
 const models = require('./models/models')
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5000
 const app = express()
 
 const start = async () => {
